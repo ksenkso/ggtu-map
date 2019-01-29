0b5740bff337647526065ee0ad19de75315bc283
ksenkso <yazunya@gmail.com>
2019-01-29 18:35:45 +0300

    Build with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'ggtu-map.js',
+        library: 'ggtuMap',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ],
+
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+};
