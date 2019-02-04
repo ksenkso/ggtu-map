919da807219ef3890223981e8d2b8a669f97e66b
ksenkso <yazunya@gmail.com>
2019-02-04 17:30:03 +0300

    Updated existing endpoints to utilize base class methods. Added `getTransitions` for the buildings endpoint.

--- src/api/endpoints/LocationsEndpoint.ts
+++ src/api/endpoints/LocationsEndpoint.ts
@@ -1,4 +1,4 @@
-import {BaseEndpoint, DeleteState, IEndpoint, IGetParams, ILocation, IPartial, IPlace} from "../common";
+import {BaseEndpoint, IEndpoint, IGetParams, ILocation, IPlace} from "../common";
 import {AxiosInstance} from "axios";
 
 export interface ILocationsEndpoint extends IEndpoint<ILocation> {
@@ -8,61 +8,12 @@ export interface ILocationsEndpoint extends IEndpoint<ILocation> {
 export default class LocationsEndpoint extends BaseEndpoint implements ILocationsEndpoint {
   protected route: string = 'locations/';
 
-  constructor(private api: AxiosInstance) {
-    super();
-  }
-
-  async create(data: ILocation): Promise<ILocation | null> {
-    const response = await this.api.post<ILocation>(this.route, data);
-    if (response.status === 201) {
-      return response.data;
-    } else {
-      return null;
-    }
-  }
-
-  async delete(id: number): Promise<DeleteState | null> {
-    const response = await this.api.delete(this.route + id);
-    if (response.status === 200) {
-      return response.data as DeleteState;
-    } else {
-      return null;
-    }
-  }
-
-  async get(id: number, params?: IGetParams): Promise<ILocation | null> {
-    const config = LocationsEndpoint.parseParams(params);
-    const response = await this.api.get<ILocation>(this.route + id, config);
-    if (response.status === 200) {
-      return response.data;
-    } else {
-      return null;
-    }
-  }
-
-  async getAll(params?: IGetParams): Promise<ILocation[] | null> {
-    const config = LocationsEndpoint.parseParams(params);
-    const response = await this.api.get<ILocation[]>(this.route, config);
-    if (response.status === 200) {
-      return response.data;
-    } else {
-      return null;
-    }
+  constructor(api: AxiosInstance) {
+    super(api);
   }
 
   async getPlaces(locationId: number, params?: IGetParams): Promise<IPlace[] | null> {
-    // const config = LocationsEndpoint.parseParams(params);
-    // console.log(config);
     const response = await this.api.get<IPlace[]>(this.route + locationId + '/places', {params});
-    if (response.status === 200) {
-      return response.data;
-    } else {
-      return null;
-    }
-  }
-
-  async update(id: number, fields: IPartial<ILocation>): Promise<ILocation | null> {
-    const response = await this.api.patch<ILocation>(this.route + id, fields);
     if (response.status === 200) {
       return response.data;
     } else {
