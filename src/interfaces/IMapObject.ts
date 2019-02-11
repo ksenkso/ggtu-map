47a707f93bcd08bb4993f30ecf4e586d2a22b556
ksenkso <yazunya@gmail.com>
2019-02-11 21:08:37 +0300

    IMapObject interface for MapObject and ISerializable for serializable classes

---
+++ src/interfaces/IMapObject.ts
@@ -0,0 +1,5 @@
+export interface IMapObject {
+  id: number;
+  PlaceId: number | null;
+  TransitionViewId: number | null;
+}
---
+++ src/interfaces/ISerializable.ts
@@ -0,0 +1,3 @@
+export default interface ISerializable {
+  serialize(): any;
+}
