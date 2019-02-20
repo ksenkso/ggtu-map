1d70eac018fe55ff1a6ae81f5933a7ac1f2393ed
ksenkso <yazunya@gmail.com>
2019-02-20 23:46:54 +0300

    still trying to implement zooming in the right way

--- src/core/controls/ZoomControl.ts
+++ src/core/controls/ZoomControl.ts
@@ -1,4 +1,5 @@
 import IScene from '../../interfaces/IScene';
+import Vector from '../../utils/Vector';
 import BaseControl from './BaseControl';
 
 export default class ZoomControl extends BaseControl {
@@ -18,10 +19,15 @@ export default class ZoomControl extends BaseControl {
         this.scene.container.addEventListener('wheel', this.onWheel.bind(this));
     }
 
-    public setZoom(f) {
-        if (f > 0 && f < 2) {
-            this.zoom = f;
-            this.scene.root.style.setProperty('--scale', f);
+    public setZoom(delta) {
+        if (this.zoom > 0.5 && this.zoom < 2) {
+            const oldScale = this.zoom;
+            this.zoom *= delta;
+            this.scene.root.style.setProperty('--scale', String(this.zoom));
+            // Recalculate labels positions
+            for (const child of this.scene.labelsContainer.children) {
+                this.setLabelPosition(child as SVGTextElement, oldScale);
+            }
         }
     }
 
@@ -30,8 +36,39 @@ export default class ZoomControl extends BaseControl {
     }
 
     private onWheel(e) {
-        const f = this.zoom + e.deltaY / 1000;
+        const rootBox = this.scene.root.getBoundingClientRect();
+        console.log(e, rootBox);
+        const px = (e.clientX - rootBox.left) / rootBox.width;
+        const py = (e.clientY - rootBox.top) / rootBox.height;
+        this.scene.root.style.setProperty('--origin', `${px * 100}% ${py * 100}%`);
+        // const coords = this.scene.getMouseCoords(e);
+        /**
+         * 1. Set the scale
+         * 2. Move the map
+         */
+        const d = 1 - (e.deltaY / 2000);
+        const f = this.zoom + d;
         console.log(f);
-        this.setZoom(f);
+        this.setZoom(d);
+        /*const viewBox = this.scene.getViewBox();
+        const delta = {
+            x: (coords.x - viewBox[0]) * (d - 1),
+            y: (coords.y - viewBox[1]) * (d - 1),
+        };
+        this.scene.setViewBox([viewBox[0] - delta.x, viewBox[1] - delta.y, viewBox[2], viewBox[3]]);*/
+    }
+
+    private setLabelPosition(child: SVGTextElement, oldScale: number) {
+        const center = {
+            x: +child.getAttribute('x'),
+            y: +child.getAttribute('y'),
+        };
+        const newCenter = Vector.scale(center, this.zoom / oldScale);
+        child.setAttribute('x', String(newCenter.x));
+        child.setAttribute('y', String(newCenter.y));
+        /*const x = +child.getAttribute('x');
+        const y = +child.getAttribute('y');
+        child.setAttribute('x', String(x / oldScale * this.zoom));
+        child.setAttribute('y', String(y / oldScale * this.zoom));*/
     }
 }
