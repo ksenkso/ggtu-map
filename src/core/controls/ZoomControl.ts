8c3914bef9d5109c8fdf4e5bb8d8bbb73ae8f46f
ksenkso <yazunya@gmail.com>
2019-02-21 17:47:19 +0300

    Zoom fixed

--- src/core/controls/ZoomControl.ts
+++ src/core/controls/ZoomControl.ts
@@ -3,11 +3,30 @@ import Vector from '../../utils/Vector';
 import BaseControl from './BaseControl';
 
 export default class ZoomControl extends BaseControl {
-    public zoom: number;
+    public static FONTSIZE_DELTA_BIG_SCALE: number = .8;
+    public static LABEL_VISIBILITY_SCALE: number = 0.75;
+    public static DELTA_FACTOR: number = 2000;
+    public static LABEL_LARGE_SCALE: number = 2;
 
-    constructor(initial = 1) {
+    public static getMatrix(scene: SVGGElement): number[] {
+        const trf = scene.style.transform;
+        if (trf) {
+            return trf.match(/matrix\((.*)\)/)[1].split(/,\s?/).map((v) => +v);
+        } else {
+            return [1, 0, 0, 1, 0, 0];
+        }
+    }
+
+    public static setMatrix(scene, matrix) {
+        scene.style.transform = `matrix(${matrix.join(',')})`;
+    }
+
+    public zoom: number;
+    private fontSize: number;
+
+    constructor() {
         super();
-        this.zoom = initial;
+        this.zoom = 1;
     }
 
     public appendTo(scene: IScene): void {
@@ -17,58 +36,47 @@ export default class ZoomControl extends BaseControl {
 
     public init() {
         this.scene.container.addEventListener('wheel', this.onWheel.bind(this));
+        const style = getComputedStyle(this.scene.root);
+        this.fontSize = parseFloat(style.getPropertyValue('--label-font-size'));
     }
-
-    public setZoom(delta) {
-        if (this.zoom > 0.5 && this.zoom < 2) {
-            const oldScale = this.zoom;
-            this.zoom *= delta;
-            this.scene.root.style.setProperty('--scale', String(this.zoom));
-            // Recalculate labels positions
-            for (const child of this.scene.labelsContainer.children) {
-                this.setLabelPosition(child as SVGTextElement, oldScale);
-            }
-        }
-    }
-
-    public getZoom() {
-        return this.zoom;
-    }
-
+    /**
+     * Scales map container
+     *
+     * In order to scale an image and keep one point fixed we need to perform
+     * scaling and translation as follows:
+     * (x', y') - new coordinates
+     * (x', y') = s * (x, y) + (u, v)
+     * if (x0, y0) is fixed, then
+     * (x0, y0) = s * (x0, y0) + (u, v);
+     * (x0, y0) - s * (x0, y0) = (u, v);
+     * (u, v) = (1 - s) * (x0, y0)
+     */
     private onWheel(e) {
-        const rootBox = this.scene.root.getBoundingClientRect();
-        console.log(e, rootBox);
-        const px = (e.clientX - rootBox.left) / rootBox.width;
-        const py = (e.clientY - rootBox.top) / rootBox.height;
-        this.scene.root.style.setProperty('--origin', `${px * 100}% ${py * 100}%`);
-        // const coords = this.scene.getMouseCoords(e);
-        /**
-         * 1. Set the scale
-         * 2. Move the map
-         */
-        const d = 1 - (e.deltaY / 2000);
-        const f = this.zoom + d;
-        console.log(f);
-        this.setZoom(d);
-        /*const viewBox = this.scene.getViewBox();
-        const delta = {
-            x: (coords.x - viewBox[0]) * (d - 1),
-            y: (coords.y - viewBox[1]) * (d - 1),
-        };
-        this.scene.setViewBox([viewBox[0] - delta.x, viewBox[1] - delta.y, viewBox[2], viewBox[3]]);*/
-    }
-
-    private setLabelPosition(child: SVGTextElement, oldScale: number) {
-        const center = {
-            x: +child.getAttribute('x'),
-            y: +child.getAttribute('y'),
-        };
-        const newCenter = Vector.scale(center, this.zoom / oldScale);
-        child.setAttribute('x', String(newCenter.x));
-        child.setAttribute('y', String(newCenter.y));
-        /*const x = +child.getAttribute('x');
-        const y = +child.getAttribute('y');
-        child.setAttribute('x', String(x / oldScale * this.zoom));
-        child.setAttribute('y', String(y / oldScale * this.zoom));*/
+        // get scale as a distance that was scrolled
+        const scale = 1 - e.deltaY / ZoomControl.DELTA_FACTOR;
+        // Scale coords with current zoom factor
+        const coords = Vector.scale(this.scene.getMouseCoords(e), this.zoom);
+        this.zoom *= scale;
+        // Transform mapContainer's matrix
+        const matrix = ZoomControl.getMatrix(this.scene.mapContainer);
+        // Scale a, b, c, d
+        for (let i = 0; i < 4; i++) {
+            matrix[i] *= scale;
+        }
+        matrix[4] += (1 - scale) * coords.x; // move x by (1 - s) * x
+        matrix[5] += (1 - scale) * coords.y; // move y by (1 - s) * y
+        ZoomControl.setMatrix(this.scene.mapContainer, matrix);
+        // If zoom level is too low to keep labels visible, hide them
+        if (this.zoom < ZoomControl.LABEL_VISIBILITY_SCALE) {
+            this.scene.mapContainer.classList.add('scene__map_no-labels');
+        } else {
+            this.scene.mapContainer.classList.remove('scene__map_no-labels');
+            let fz = this.fontSize;
+            if (this.zoom > ZoomControl.LABEL_LARGE_SCALE) {
+                // Increase font size on big scales
+                fz += ZoomControl.FONTSIZE_DELTA_BIG_SCALE;
+            }
+            this.scene.root.style.setProperty('--label-font-size', fz / this.zoom + 'px');
+        }
     }
 }
