697cc5799208f9b10a8797d7d7a0ebe3a2cfdf44
ksenkso <yazunya@gmail.com>
2019-02-21 17:47:04 +0300

    file reformatted; now labels are appended to the element supplied

--- src/core/Scene.ts
+++ src/core/Scene.ts
@@ -12,236 +12,233 @@ import DragControl from './controls/DragControl';
 import ZoomControl from './controls/ZoomControl';
 import ObjectManager from './ObjectManager';
 import Selection from './Selection';
+
 export interface IMapMouseEvent {
-  originalEvent: MouseEvent;
-  mapCoords: ICoords;
-  objectType?: ObjectType;
-  mapObject?: MapObject;
-  mapElement?: SVGGElement;
+    originalEvent: MouseEvent;
+    mapCoords: ICoords;
+    objectType?: ObjectType;
+    mapObject?: MapObject;
+    mapElement?: SVGGElement;
 }
 
 export default class Scene extends EventEmitter implements IScene {
 
-  /*public get pointsContainer() {
-    return this.activePath.querySelector('.path__points');
-  }
-  public get linesContainer() {
-    return this.activePath.querySelector('.path__lines');
-  }*/
+    /**
+     * Sets a label for an object on the map
+     * @param el
+     * @param name
+     */
+    public static setLabel(el: SVGGraphicsElement | ICoords, name: string) {
+        const text = Primitive.createElement('text') as SVGTextElement;
+        text.classList.add('primitive_label');
 
-  public readonly apiClient: ApiClient;
-  public readonly selection: Selection;
-  public readonly objectManager: ObjectManager;
-  public readonly dragManager: DragManager;
-  public readonly interactable: Interactable;
-  // private pathsContainer: SVGGElement;
+        text.textContent = name;
+        text.setAttribute('text-anchor', 'middle');
+        text.setAttribute('dominant-baseline', 'middle');
 
-  public drawingContainer: SVGGElement;
-  public mapContainer: SVGGElement;
-  public controlsContainer: SVGElement;
-  public labelsContainer: SVGGElement;
-  public root: SVGSVGElement;
-  public container: HTMLElement;
-
-  private _location: ILocation;
-  // public activePath: SVGGElement;
-
-  private svg: SVGSVGElement;
-
-  constructor(
-    container: HTMLElement,
-  ) {
-    super();
-
-    this.container = container;
-    this.root = Primitive.createElement('svg', false) as SVGSVGElement;
-    this.root.classList.add('map__root');
-    this.container.appendChild(this.root);
-    // Create containers for map and paths
-    this.mapContainer = Graphics.createElement('g', false) as SVGGElement;
-    this.mapContainer.classList.add('scene__map');
-    this.root.appendChild(this.mapContainer);
-    this.drawingContainer = Graphics.createElement('g', false) as SVGGElement;
-    this.drawingContainer.classList.add('scene__drawing');
-    this.root.appendChild(this.drawingContainer);
-    this.controlsContainer = Graphics.createElement('g', false) as SVGGElement;
-    this.root.appendChild(this.controlsContainer);
-    this.labelsContainer = Graphics.createElement('g', false) as SVGGElement;
-    this.labelsContainer.classList.add('map__labels');
-    this.root.appendChild(this.labelsContainer);
-    this.interactable = interact(this.container);
-    const drag = new DragControl();
-    drag.appendTo(this);
-    const zoom = new ZoomControl();
-    zoom.appendTo(this);
-    this.apiClient = ApiClient.getInstance();
-    this.selection = new Selection();
-    this.objectManager = new ObjectManager(this.apiClient);
-    this.dragManager = new DragManager(this);
-    this.container.addEventListener('click', this.onMapClick.bind(this));
-    this.container.addEventListener('keyup', this.onKeyUp.bind(this));
-  }
-
-  /**
-   * Sets a label for an object on the map
-   * @param el
-   * @param name
-   */
-  public setLabel(el: SVGGraphicsElement | ICoords, name: string) {
-    const text = Primitive.createElement('text') as SVGTextElement;
-    text.classList.add('primitive_label');
-
-    text.textContent = name;
-    text.setAttribute('text-anchor', 'middle');
-    text.setAttribute('dominant-baseline', 'middle');
-
-    let x, y;
-    if (el instanceof SVGGraphicsElement) {
-      // get the box before text element is appended, otherwise it will extend the box to the top-left corner of the SVG
-      const rect = el.getBBox();
-      this.labelsContainer.appendChild(text);
-      // const textRect = text.getBBox();
-      /*x = rect.x + (rect.width - textRect.width) / 2;
-      y = rect.y + (rect.height + textRect.height) / 2;*/
-      x = rect.x + rect.width / 2;
-      y = rect.y + rect.height / 2;
-    } else {
-      x = el.x;
-      y = el.y;
+        let x, y;
+        if (el instanceof SVGGraphicsElement) {
+            // get the box before text element is appended,
+            // otherwise it will extend the box to the top-left corner of the SVG
+            const rect = el.getBBox();
+            el.appendChild(text);
+            x = rect.x + rect.width / 2;
+            y = rect.y + rect.height / 2;
+        } else {
+            x = el.x;
+            y = el.y;
+        }
+        text.setAttribute('x', String(x));
+        text.setAttribute('y', String(y));
     }
-    text.setAttribute('x', String(x));
-    text.setAttribute('y', String(y));
 
-  }
+    /*public get pointsContainer() {
+      return this.activePath.querySelector('.path__points');
+    }
+    public get linesContainer() {
+      return this.activePath.querySelector('.path__lines');
+    }*/
 
-  public getLocation(): ILocation {
-    return this._location;
-  }
+    public readonly apiClient: ApiClient;
+    public readonly selection: Selection;
+    public readonly objectManager: ObjectManager;
+    public readonly dragManager: DragManager;
+    public readonly interactable: Interactable;
+    // private pathsContainer: SVGGElement;
 
-  public setLocation(value: ILocation, force = false) {
-    if (force || (!this._location || this._location.id !== value.id)) {
-      this._location = value;
-      if (value.map) {
-        this.apiClient.getTransport().get(ApiClient.mapsBase + '/' + value.map)
-          .then((response) => {
-            if (response.status === 200) {
-              this.setMapFromString(response.data as string);
-              this.objectManager.updateLocation(value.id)
-                .then(() => {
-                  const objects: any[] = (this.objectManager.places as any[]).concat(this.objectManager.buildings);
-                  for (const object of objects) {
-                    const selector = '#' + CSS.escape(object.container);
-                    const el = this.mapContainer.querySelector(selector) as SVGGraphicsElement;
-                    if (el) {
-                      console.log(selector);
-                      this.setLabel(el, object.name);
-                    }
-                  }
-                });
+    public drawingContainer: SVGGElement;
+    public mapContainer: SVGGElement;
+    public controlsContainer: SVGElement;
+    public labelsContainer: SVGGElement;
+    public root: SVGSVGElement;
+    public container: HTMLElement;
+
+    private _location: ILocation;
+    // public activePath: SVGGElement;
+
+    private svg: SVGSVGElement;
+
+    constructor(
+        container: HTMLElement,
+    ) {
+        super();
+
+        this.container = container;
+        this.root = Primitive.createElement('svg', false) as SVGSVGElement;
+        this.root.classList.add('map__root');
+        this.container.appendChild(this.root);
+        // Create containers for map and paths
+        this.mapContainer = Graphics.createElement('g', false) as SVGGElement;
+        this.mapContainer.classList.add('scene__map');
+        this.root.appendChild(this.mapContainer);
+        this.drawingContainer = Graphics.createElement('g', false) as SVGGElement;
+        this.drawingContainer.classList.add('scene__drawing');
+        this.root.appendChild(this.drawingContainer);
+        this.controlsContainer = Graphics.createElement('g', false) as SVGGElement;
+        this.root.appendChild(this.controlsContainer);
+        this.labelsContainer = Graphics.createElement('g', false) as SVGGElement;
+        this.labelsContainer.classList.add('map__labels');
+        this.root.appendChild(this.labelsContainer);
+        this.interactable = interact(this.container);
+        const drag = new DragControl();
+        drag.appendTo(this);
+        const zoom = new ZoomControl();
+        zoom.appendTo(this);
+        this.apiClient = ApiClient.getInstance();
+        this.selection = new Selection();
+        this.objectManager = new ObjectManager(this.apiClient);
+        this.dragManager = new DragManager(this);
+        this.container.addEventListener('click', this.onMapClick.bind(this));
+        this.container.addEventListener('keyup', this.onKeyUp.bind(this));
+    }
+
+    public getLocation(): ILocation {
+        return this._location;
+    }
+
+    public setLocation(value: ILocation, force = false) {
+        if (force || (!this._location || this._location.id !== value.id)) {
+            this._location = value;
+            if (value.map) {
+                this.apiClient
+                    .getTransport()
+                    .get(ApiClient.mapsBase + '/' + value.map)
+                    .then((response) => {
+                        if (response.status === 200) {
+                            this.setMapFromString(response.data as string);
+                            return this.objectManager.updateLocation(value.id);
+                        }
+                    })
+                    .then(() => {
+                        const objects: any[] = (this.objectManager.places as any[])
+                            .concat(this.objectManager.buildings);
+                        for (const object of objects) {
+                            const selector = '#' + CSS.escape(object.container);
+                            const el = this.mapContainer.querySelector(selector) as SVGGraphicsElement;
+                            if (el) {
+                                console.log(selector);
+                                Scene.setLabel(el, object.name);
+                            }
+                        }
+                    })
+                    .catch((error) => {
+                        //  TODO: create an error handling system
+                        console.error(error);
+                    });
+            } else {
+                throw new Error('Map is not available');
             }
-          })
-          .catch((error) => {
-            //  TODO: create an error handling system
-            console.error(error);
-          });
-      } else {
-        throw new Error('Map is not available');
-      }
-    }
-    this._location = value;
-  }
-
-  public setMapFromString(map: string) {
-    const parser = new DOMParser();
-    const dom = parser.parseFromString(map, 'image/svg+xml');
-    const root = dom.firstElementChild;
-    this.updateMap(root as SVGSVGElement);
-  }
-
-  /*public setMapFromFile(file: File): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      if (file.type !== 'image/svg+xml') {
-        reject(new Error('Map file should be an SVG with MIME-type "image/svg+xml, got ' + file.type));
-      }
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        if (reader.readyState === FileReader.DONE) {
-          this.setMapFromString(reader.result as string);
-          resolve();
         }
-      };
-      reader.readAsText(file);
-    })
-  }*/
+        this._location = value;
+    }
 
-  public getMouseCoords(e: MouseEvent): ICoords {
-    const bounds = this.mapContainer.getBoundingClientRect();
-    const zoomLevel = this.getZoomLevel(bounds);
-    const x = (e.clientX - bounds.left) / zoomLevel;
-    const y = (e.clientY - bounds.top) / zoomLevel;
-    return {x, y};
-  }
+    public setMapFromString(map: string) {
+        const parser = new DOMParser();
+        const dom = parser.parseFromString(map, 'image/svg+xml');
+        const root = dom.firstElementChild;
+        this.updateMap(root as SVGSVGElement);
+    }
 
-  public getViewBox(): number[] {
-    return this.root.getAttribute('viewBox').split(' ').map((v) => +v);
-  }
-
-  public setViewBox(viewBox: number[]): void {
-    this.root.setAttribute('viewBox', viewBox.join(' '));
-  }
-
-  public getZoomLevel(bounds: ClientRect): number {
-    const viewBox = this.getViewBox();
-    return bounds.width / viewBox[2];
-  }
-
-  public async refresh(): Promise<void> {
-    const updated = await this.apiClient.locations.get(this._location.id);
-    this.setLocation(updated, true);
-  }
-
-  private onMapClick(event: MouseEvent): void {
-    const payload: IMapMouseEvent = {
-      mapCoords: this.getMouseCoords(event),
-      originalEvent: event,
-    };
-    // Find an object in the event path:
-    const path = event.composedPath().filter((el) => el !== window && el !== document);
-    console.log(path);
-    const mapElement = path.find((target: Element) => target.matches('g[data-type]')) as SVGGElement;
-    if (mapElement) {
-      const type = mapElement.dataset.type as ObjectType;
-      payload.objectType = type;
-      if (mapElement) {
-        payload.mapElement = mapElement;
-        const id = +mapElement.dataset.id;
-        if (id) {
-          // Check if we have this object.
-          payload.mapObject = this.objectManager.getCollectionByType(type).find((item) => item.id === id);
+    /*public setMapFromFile(file: File): Promise<void> {
+      return new Promise<void>((resolve, reject) => {
+        if (file.type !== 'image/svg+xml') {
+          reject(new Error('Map file should be an SVG with MIME-type "image/svg+xml, got ' + file.type));
         }
-      }
-    }
-    this.emit('click', payload);
-  }
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          if (reader.readyState === FileReader.DONE) {
+            this.setMapFromString(reader.result as string);
+            resolve();
+          }
+        };
+        reader.readAsText(file);
+      })
+    }*/
 
-  private updateMap(map: SVGSVGElement): void {
-    if (this.svg) {
-      this.svg.remove();
+    public getMouseCoords(e: MouseEvent): ICoords {
+        const bounds = this.mapContainer.getBoundingClientRect();
+        const zoomLevel = this.getZoomLevel(bounds);
+        const x = (e.clientX - bounds.left) / zoomLevel;
+        const y = (e.clientY - bounds.top) / zoomLevel;
+        return {x, y};
     }
-    const viewBox = map.getAttribute('viewBox');
-    if (viewBox) {
-      this.root.setAttribute('viewBox', viewBox);
-    }
-    this.svg = map;
-    this.mapContainer.innerHTML = map.innerHTML;
-    // TODO: change map structure to render UI and primitives
-    /*this.pathsContainer.innerHTML = '';
-    this.activePath = Scene.createPath();
-    this.pathsContainer.appendChild(this.activePath);*/
-    this.emit('mapChanged');
-  }
 
-  private onKeyUp(e) {
-    this.emit('keyup', e);
-  }
+    public getViewBox(): number[] {
+        return this.root.getAttribute('viewBox').split(' ').map((v) => +v);
+    }
+
+    public setViewBox(viewBox: number[]): void {
+        this.root.setAttribute('viewBox', viewBox.join(' '));
+    }
+
+    public getZoomLevel(bounds: ClientRect): number {
+        const viewBox = this.getViewBox();
+        return bounds.width / viewBox[2];
+    }
+
+    public async refresh(): Promise<void> {
+        const updated = await this.apiClient.locations.get(this._location.id);
+        this.setLocation(updated, true);
+    }
+
+    private onMapClick(event: MouseEvent): void {
+        const payload: IMapMouseEvent = {
+            mapCoords: this.getMouseCoords(event),
+            originalEvent: event,
+        };
+        // Find an object in the event path:
+        const path = event.composedPath().filter((el) => el !== window && el !== document);
+        console.log(path);
+        const mapElement = path.find((target: Element) => target.matches('g[data-type]')) as SVGGElement;
+        if (mapElement) {
+            const type = mapElement.dataset.type as ObjectType;
+            payload.objectType = type;
+            if (mapElement) {
+                payload.mapElement = mapElement;
+                const id = +mapElement.dataset.id;
+                if (id) {
+                    // Check if we have this object.
+                    payload.mapObject = this.objectManager.getCollectionByType(type).find((item) => item.id === id);
+                }
+            }
+        }
+        this.emit('click', payload);
+    }
+
+    private updateMap(map: SVGSVGElement): void {
+        if (this.svg) {
+            this.svg.remove();
+        }
+        const viewBox = map.getAttribute('viewBox');
+        if (viewBox) {
+            this.root.setAttribute('viewBox', viewBox);
+        }
+        this.svg = map;
+        this.mapContainer.innerHTML = map.innerHTML;
+        this.emit('mapChanged');
+    }
+
+    private onKeyUp(e) {
+        this.emit('keyup', e);
+    }
 }
