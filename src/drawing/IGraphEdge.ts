c1b927306e13da3a390efdde698a734818537479
ksenkso <yazunya@gmail.com>
2019-02-24 16:07:47 +0300

    exposed `start` and `end` fields

--- src/drawing/IGraphEdge.ts
+++ src/drawing/IGraphEdge.ts
@@ -1,7 +1,10 @@
 import {ILine} from '..';
+import IGraphPoint from '../interfaces/IGraphPoint';
 import {IGraph} from './Graph';
 
 export interface IGraphEdge extends ILine {
+  start: IGraphPoint;
+  end: IGraphPoint;
   update(): void;
   setGraph(graph: IGraph): void;
 }
