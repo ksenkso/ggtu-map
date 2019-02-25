4f6368d48a647255b25b720997f45173d2b0e72a
ksenkso <yazunya@gmail.com>
2019-02-25 22:29:06 +0300

    element searching algorithm changed to work with events triggered on any element

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,9 +1,17 @@
-export function getMapElementAtCoords(x: number, y: number): SVGGElement {
-    let parent = document.elementFromPoint(x, y).parentElement;
+
+export function getMapElementAtCoords(container: Element, x: number, y: number): SVGGElement {
+    const containers = container.querySelectorAll('g[data-type]');
+    for (let i = 0; i < containers.length; i++) {
+        const box = (containers[i] as SVGGElement).getBBox();
+        if (box.x < x && (box.x + box.width) > x && box.y < y && (box.y + box.height) > y) {
+            return containers[i] as SVGGElement;
+        }
+    }
+    /*let parent = document.elementFromPoint(x, y).parentElement;
     if (parent) {
         while (parent && !parent.matches('g[data-type]')) {
             parent = parent.parentElement;
         }
         return (parent as any) as SVGGElement;
-    }
+    }*/
 }
